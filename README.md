b31478aa76f8578e4185814663ac4550598975a8
constipur <qiaoy18@mails.tsinghua.edu.cn>
2018-11-22 01:33:57 +0000

    Renaming

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # NetEC
-Attempting to offload erasure coding (entirely or partially) to programmable forwarding engines for greater performance and saclability
---
+++ qy_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#created 2018.3 qiaoyi
+#modified from wsh's script
+#update 2018.11 qiaoyi to port from bf-sde-4-* to bf-sde-8-*
+#update 2018.11 qiaoyi for NetEC
+
+#build synproxy program
+
+cd $SDE/pkgsrc/p4-build
+make clean
+echo 'clean done!'
+./configure --prefix=$SDE_INSTALL --with-tofino enable_thrift=yes P4_PATH=$SDE/NetEC/netec.p4 P4_NAME=netec
+
+make -j8
+echo 'make done!'
+make install
+echo 'make install done!'
+sed -e "s/TOFINO_SINGLE_DEVICE/qy/" $SDE/pkgsrc/p4-examples/tofino/tofino_single_device.conf.in > $SDE_INSTALL/share/p4/targets/tofino/netec.conf
+echo 'conf done!'
